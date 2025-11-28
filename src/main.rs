41c7014ca5125e288db5f33211a96c8d1c725345
Kirby0717 <niconicoyusei@gmail.com>
2025-11-28 19:31:30 +0900

    init

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+fn main() -> anyhow::Result<()> {
+    welcome();
+
+    let current_dir = std::env::current_dir()?;
+    loop {
+        continuation(&current_dir);
+
+        let stdin = std::io::stdin();
+        let mut line = String::new();
+        match stdin.read_line(&mut line) {
+            Ok(_len) => {
+                println!("{line:?}");
+            }
+            Err(e) => {
+                eprintln!("err: {e}");
+            }
+        }
+    }
+}
+
+fn welcome() {
+    println!("Welcome to Asari!");
+}
+
+fn continuation(current_dir: &std::path::Path) {
+    use std::io::Write;
+    print!("{}>", format_path(current_dir));
+    std::io::stdout().flush().expect("stdoutのフラッシュに失敗");
+}
+
+fn format_path(path: &std::path::Path) -> String {
+    if let Some(home) = std::env::home_dir()
+        && let Ok(relative) = path.strip_prefix(&home)
+    {
+        if path == home {
+            "~".to_string()
+        }
+        else {
+            format!("~/{}", relative.display())
+        }
+    }
+    else {
+        path.display().to_string()
+    }
+}
